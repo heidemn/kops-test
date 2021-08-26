b873918ebe0baa4953abc093d8b0b8be88a83877
Martin Heide <martin.heide@gmx.net>
2021-08-27 00:22:50 +0200

    Cleanup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
+exit
+
+
 curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.22.0/bin/linux/amd64/kubectl
 chmod +x kubectl
 sudo mv kubectl /usr/local/bin/kubectl
-exit
 
 curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x kops
@@ -20,8 +22,6 @@ aws s3api put-bucket-versioning --bucket kops-martinowitsch-state \
 
 aws s3api put-bucket-encryption --bucket kops-martinowitsch-state \
     --server-side-encryption-configuration '{"Rules":[{"ApplyServerSideEncryptionByDefault":{"SSEAlgorithm":"AES256"}}]}'
-
-export NAME=myfirstcluster.example.com
 
 # ./kops.sh edit cluster martinowitsch.net
 # ./kops.sh edit ig --name=martinowitsch.net master-eu-west-1a
